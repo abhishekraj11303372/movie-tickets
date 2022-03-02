02d35b03cd193325bb6b1e037e41c1544c972992
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-02 14:26:32 +0530

    Nested Route and Props with v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { BrowserRouter,Routes, Route, Link, useParams} from "react-router-dom";
 import Home from './components/Home';
 import Admin from './components/Admin';
 import Movies from './components/Movies';
+import Categories from './components/Categories';
 
 export default function App() {
   return (
@@ -27,16 +27,30 @@ export default function App() {
               <Link to="/movies">Movies</Link>
               </li>
               <li className="list-group-item">
+              <Link to="/by-category">Categories</Link>
+              </li>
+              <li className="list-group-item">
               <Link to="/admin">Movie Catalog</Link>
               </li>
             </ul>
           </nav>
         </div>
         <div className="col-md-10">
-          <Routes>
-          <Route path="/" element={<><Home /></>} ></Route>
+          <Routes>  
+          <Route path="/" element={<Home />} ></Route>
             <Route path="/movies" element={<><Movies /></>} ></Route>
             <Route path="/movies/:id" element={<><Movie /></>} ></Route>
+            <Route exact path="/by-category/*" element={<CategoryPage />} >
+              <Route path="action" element={<p>Action page</p>}/>
+              <Route path="comedy" element={<p>Comedy Page</p>}/>
+              <Route path="drama" element={<p>Drama page</p>}/>
+            </Route>
+            <Route exact path="/by-category/drama" element={<Categories title={`Drama`}/>} />
+            <Route exact path="/by-category/comedy" element={ <Categories title={`Comedy`}/>}  />
+            <Route exact path="/by-category/action" element={<Categories title={`Action`}/>} />
+            {/* <Route exact path="/by-category/drama" render={(props) => <Categories {...props} title={`Drama`}/>}  element={<Categories />}></Route>
+            <Route path="/by-category/comedy" render={(props) => <Categories {...props} title={`Comedy`}/>} element={<><Categories /></>} ></Route>
+            <Route path="/by-category/action" render={(props) => <Categories {...props} title={`Action`}/>} element={<><Categories /></>} ></Route> */}
             <Route path="/admin" element={<><Admin /></>} ></Route>
           </Routes>
         </div>
@@ -50,3 +64,25 @@ function Movie() {
   let { id } = useParams();
   return <h2>Movie id {id}</h2> 
 }
+
+function CategoryPage() {
+
+  return (
+    <div>
+      <h2>Categories</h2>
+      
+      <ul>
+        <li><Link to="action">Action</Link></li>
+        <li><Link to="comedy">Comedy</Link></li>
+        <li><Link to="drama">Drama</Link></li>
+        {/* <Outlet/> */}
+        
+      </ul>
+    </div>
+  ) 
+}
+
+// function MovieCategorised() {
+  
+//   return <h2>Category: {this.props.title}</h2>  
+// }
