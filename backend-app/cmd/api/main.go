04ac74ef38c6e69c3414630abe84bdb836f7b185
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-10 12:22:39 +0530

    init backend

---
+++ backend-app/cmd/api/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type AppStatus struct {
+	Status     string `json:"status"`
+	Enviroment string `json:"enviroment"`
+	Version    string `json:"version"`
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application enviroment (development|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
+
+	fmt.Printf("Running")
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Fprint(rw, "status")
+		currentStatus := AppStatus{
+			Status:     "Available",
+			Enviroment: cfg.env,
+			Version:    version,
+		}
+
+		js, err := json.MarshalIndent(currentStatus, "", "\t")
+		if err != nil {
+			log.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(js)
+
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
